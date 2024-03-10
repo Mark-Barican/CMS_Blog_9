c40e5d3eed05813a0b9de02057ac221de9c4f0f5
AbsoluteZero2718 <jeremydaingonzales84@gmail.com>
2024-03-10 20:50:39 +0800

    CMS_Blog_9 v.1.0.1
    - Added User Management
    - Added new users.php file
    - Changed admin password from 'secret' to 'admin'
    - Made edits to dashboard.php file

--- dashboard.php
+++ dashboard.php
@@ -9,8 +9,11 @@ include('includes/header.php');
 ?>
 <div class="container mt-5">
   <div class="row justify-content-center">
+    <h1 class="dispaly-1">Dashboard</h1>
     <div class="col-md-6">
-      Dashboard
+      <a href = "users.php">User Management</a> |
+      <a href = "posts.php">Post Management</a>
+      
     </div>
   </div>
 </div>
--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'Blog_9', 'secretpassword', 'Blog_9');
+$connect = mysqli_connect('localhost', 'blog_9', 'admin', 'blog_9');
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
