4b5ecf8a125c72347492361644d3cd04476bc24b
seraifh <74619118+Beetle-Boombox@users.noreply.github.com>
2024-03-11 22:00:20 +0800

    added user_add.php and user_edit.ph, added stms to db

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,18 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'blog_9', 'admin', 'blog_9');
+$servername = 'localhost';
+$username = 'blog_9_1';
+$password = 'admin';
+$database = 'blog_9_1';
 
+$connect = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
 if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
+// Connected successfully
+echo 'Connected successfully';
 
 ?>
